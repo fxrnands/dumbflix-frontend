df1f24d1f8efdf1b697d730fb6dadb87f5b1963c
fxrnands <fxrnands@gmail.com>
2022-09-26 02:10:51 +0700

    revisi

--- src/pages/user/Upgrade.js
+++ src/pages/user/Upgrade.js
@@ -22,7 +22,7 @@ function Upgrade() {
     //change this to the script source you want to load, for example this is snap.js sandbox env
     const midtransScriptUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
     //change this according to your client-key
-    const myMidtransClientKey = "SB-Mid-client-on6S64ToFp0HbEkn";
+    const myMidtransClientKey = process.env.REACT_APP_MIDTRANS_CLIENT_KEY;
 
     let scriptTag = document.createElement("script");
     scriptTag.src = midtransScriptUrl;
