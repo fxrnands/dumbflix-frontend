2453fed2481652b612a1441e5c6b379126b7ad26
fxrnands <fxrnands@gmail.com>
2022-09-26 16:12:19 +0700

    revisi

--- src/pages/admin/AdDetailPage.js
+++ src/pages/admin/AdDetailPage.js
@@ -1,31 +1,27 @@
 import React, { useState } from "react";
 import { Button, Carousel } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { API } from "../../config/api";
+import { useQuery } from "react-query";
 import AddEpisode from "../../components/AddEpisode";
-import movies from "../../dummyData/movies";
-import tvSeries from "../../dummyData/tvseries";
 
 function AdDetailPage() {
-  const category = { movies, tvSeries };
-
   const [show, setShow] = useState(false);
   const handleShow = () => setShow(true);
   const handleClose = () => setShow(false);
 
-  const [dataCategory, setDataCategory] = useState(category);
+  let { id } = useParams();
+  let { data: film } = useQuery("productCache", async () => {
+    const response = await API.get("/film/" + id);
+    return response.data.data;
+  });
 
   return (
     <div>
       <div className="video-control">
-        <iframe
-          src="https://www.youtube.com/embed/TcMBFSGVi1c"
-          allow="autoplay; encrypted-media"
-          allowFullScreen
-          title="video"
-          width="900px"
-          height="300px"
-        />
+        <iframe src={film?.linkFilm} allow="autoplay; encrypted-media" allowFullScreen title="video" width="900px" height="300px" />
       </div>
-      <div style={{ backgroundColor: "black" }}>
+      {/* <div style={{ backgroundColor: "black" }}>
         <div className="d-grid gap-2 d-md-flex justify-content-md-end">
           <Button className="btn-addepisode" onClick={handleShow}>
             Add Episode
@@ -33,21 +29,17 @@ function AdDetailPage() {
 
           <AddEpisode show={show} handleClose={handleClose} />
         </div>
-      </div>
+      </div> */}
       <div className="detail-bot">
         <div className="detail-desc">
           <div className="img-mov me-3">
-            <img
-              src="https://i.ytimg.com/vi/ePpJDKfRAyM/movieposter.jpg"
-              alt=""
-              width="100%"
-            />
+            <img src={film?.thumbnailFilm} alt="" width="100%" />
           </div>
           <div className="desc-mov">
-            <h2>Avengers: End Game</h2>
+            <h2>{film?.title}</h2>
             <div className="d-flex text-muted">
-              <p style={{ padding: "3px" }}>2019 </p>
-              <p className="ms-3 txt-mtd">{dataCategory.movies[3].category}</p>
+              <p style={{ padding: "3px" }}>{film?.year} </p>
+              <p className="ms-3 txt-mtd">{film?.category?.name}</p>
             </div>
             <p
               className=""
@@ -56,39 +48,35 @@ function AdDetailPage() {
                 width: "80%",
               }}
             >
-              Geralt of Rivia, a solitary monster hunter, struggles to find his
-              place in a world where people often prove more wicked than beast
+              {film?.desc}
             </p>
           </div>
         </div>
-        <div
-          style={{ backgorundColor: "red" }}
-          className="d-flex justify-content-center"
-        >
-          <Carousel className="detail-play-ad">
-            <Carousel.Item>
-              <img
-                width="100%"
-                src="https://ichef.bbci.co.uk/news/640/cpsprodpb/BF0D/production/_106090984_2e39b218-c369-452e-b5be-d2476f9d8728.jpg"
-                alt="First episode"
-              />
-              <Carousel.Caption>
-                <h5>In Play Now</h5>
-              </Carousel.Caption>
-              <p>Episode 1</p>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                width="100%"
-                src="https://ichef.bbci.co.uk/news/640/cpsprodpb/BF0D/production/_106090984_2e39b218-c369-452e-b5be-d2476f9d8728.jpg"
-                alt="First episode"
-              />
-              <Carousel.Caption>
-                <h5>In Play Now</h5>
-              </Carousel.Caption>
-              <p>Episode 2</p>
-            </Carousel.Item>
-          </Carousel>
+        <div className="detail-play">
+          <div
+            className="img-in-play mt-1"
+            style={{
+              backgroundImage: `linear-gradient(to bottom, rgba(104, 106, 116, 0), rgba(0, 0, 0, 0.99)),url(${film?.thumbnailFilm})`,
+              backgroundPosition: "center center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+          >
+            <div className="d-flex justify-content-center align-items-end" style={{ width: "100%", marginTop: "10px" }}>
+              <p
+                style={{
+                  fontWeight: "700",
+                  textAlign: "center",
+                }}
+              >
+                In Play Now <br />
+                <br />
+                <br />
+                <span className="text-muted"> {film?.title}</span>
+                <br />
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
